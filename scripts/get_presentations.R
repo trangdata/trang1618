843d8feb6ff2a7a02a18f656e39f79b4a82c1d5c
Trang Le <grixor@gmail.com>
2020-08-04 16:35:25 -0400

    update presentations

--- scripts/get_presentations.R
+++ scripts/get_presentations.R
@@ -1,30 +1,34 @@
 library(dplyr)
-library(googlesheets)
+library(googlesheets4)
 library(readr)
+library(forcats)
+
+token <- gs4_auth()
 
 pre_df <-
-  gs_url(
-    "https://docs.google.com/spreadsheets/d/1CNU9vaPXlMWoNgoc2b4yBKqIwz12DyYWr-_z5cYeoL0/edit#gid=0",
-    verbose = FALSE
+  # gs_url(
+  #   ,
+  #   verbose = FALSE
+  # ) %>%
+  read_sheet("https://docs.google.com/spreadsheets/d/1CNU9vaPXlMWoNgoc2b4yBKqIwz12DyYWr-_z5cYeoL0/edit#gid=0",
+    #          col_types = cols(
+    # type = col_character(),
+    # venue = col_character(),
+    # subtype = col_character(),
+    # who = col_character(),
+    # description = col_character(),
+    # short_name = col_character(),
+    # slides = col_character(),
+    # year = col_integer(),
+    # invited = col_logical(),
+    # manual = col_logical(),
+    # start = col_datetime(format = ""),
+    # end = col_datetime(format = ""),
+    # abstract = col_character(),
+    # date = col_character(),
+    # long_description = col_character(),
+    # exurl = col_character())
   ) %>%
-  gs_read(col_types = cols(
-    type = col_character(),
-    venue = col_character(),
-    subtype = col_character(),
-    who = col_character(),
-    description = col_character(),
-    short_name = col_character(),
-    slides = col_character(),
-    year = col_integer(),
-    invited = col_logical(),
-    manual = col_logical(),
-    start = col_datetime(format = ""),
-    end = col_datetime(format = ""),
-    abstract = col_character(),
-    date = col_character(),
-    long_description = col_character(),
-    exurl = col_character()
-  )) %>%
   arrange(invited, desc(year)) %>%
   mutate(full_name = paste(short_name, year, sep = '_'))
 
